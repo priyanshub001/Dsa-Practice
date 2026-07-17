8123a0cc0379993558a7b016fb5c9bc2e957d574
Priyanshu Bhandare <129767248+priyanshub001@users.noreply.github.com>
2026-07-17 14:37:11 +0530

    Time: 23 ms (29.85%), Space: 46.7 MB (23.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -15,41 +15,41 @@ class Solution {
         }
 
         HashMap<Character,Integer> map2 = new HashMap<>();
-        int k = s1.length();
+        
+        int n = s1.length();
+        int left = 0;
 
-        for(int i = 0; i<k; i++){
+        for(int i = 0; i<s2.length(); i++){
+
             char ch = s2.charAt(i);
 
             map2.put(ch,map2.getOrDefault(ch,0)+1);
-        }
 
-        if(map1.equals(map2)) return true;
 
-        for(int i = k; i<s2.length(); i++){
+            if(i-left+1 == n){
+                if(map1.equals(map2)) return true;
+            
 
-            char add = s2.charAt(i);
 
-            map2.put(add, map2.getOrDefault(add, 0) + 1);
+            char remove = s2.charAt(left);
 
-            char rem = s2.charAt(i-k);
+            // reduce a count...
 
-            map2.put(rem, map2.get(rem) - 1);
+            map2.put(remove,map2.get(remove)-1);
 
-            if(map2.get(rem)==0){
-                map2.remove(rem);
+            if(map2.get(remove)==0){
+                map2.remove(remove);
+            }
+
+            left++;
+
             }
 
 
-            if(map2.equals(map1)) return true;
 
-
-
-        }
-       
         
+        }
 
         return false;
-
-
     }
 }
