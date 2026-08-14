835c8e3bf3cd727194bfc8a761cd3f90f13be8d4
Priyanshu Bhandare <129767248+priyanshub001@users.noreply.github.com>
2026-08-14 20:12:21 +0530

    Time: 1 ms (100%), Space: 43.4 MB (79.53%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,39 +1,63 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
 
-        int len = 0;
-        int left = 0;
+        // int len = 0;
+        // int left = 0;
 
-        HashMap<Character , Integer> map = new HashMap<>();
+        // HashMap<Character , Integer> map = new HashMap<>();
+
+        // for(int i = 0; i < s.length(); i++){
+
+        //     char ch = s.charAt(i);
+
+        //     map.put(ch, map.getOrDefault(ch, 0)+1);
+
+
+        //     while(map.get(ch) > 2){
+
+                
+
+
+        //         char leftchar = s.charAt(left);
+
+        //         map.put(leftchar,map.get(leftchar) - 1);
+
+        //         left++;
+
+        //     }
+
+        //         int count = i - left + 1;
+        //         len = Math.max(count,len);
+
+
+
+        // }
+
+        // return len;
+
+        int pk[] = new int[26];
+        int left = 0;
+        int maxlen = 0;
 
         for(int i = 0; i < s.length(); i++){
 
             char ch = s.charAt(i);
 
-            map.put(ch, map.getOrDefault(ch, 0)+1);
+            pk[ch - 'a']++;
 
-
-            while(map.get(ch) > 2){
-
-                
-
-
+            while(pk[ch - 'a'] > 2){
                 char leftchar = s.charAt(left);
 
-                map.put(leftchar,map.get(leftchar) - 1);
-
+                pk[leftchar - 'a'] --;
                 left++;
-
             }
 
-                int count = i - left + 1;
-                len = Math.max(count,len);
+            int len = i - left +1;
 
-
-
+            maxlen = Math.max(len,maxlen);
         }
 
-        return len;
-        
+
+        return maxlen;
     }
 }
